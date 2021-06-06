a7ee1fcac8d1d7becf6d75cc6e5894c96a1881c8
arurama <arurama@gmail.com>
2021-06-06 11:33:44 +0530

    Create main.cpp
    Print the Board Using the State Enum

---
+++ CCPND/IntroCPP/Printed_Board/main.cpp
@@ -0,0 +1,70 @@
+
+//
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+enum class State { Kempty, Kobstcale };
+
+vector<State> ParseLine(string line) {
+	vector <State> type;
+ 	istringstream sline(line);
+	int n;
+	char c;
+	while (sline >> n >> c) {  
+		if (n == 0) {
+			type.push_back(State::Kempty);
+		}
+		else {
+			type.push_back(State::Kobstcale);
+		}
+	}
+	return type;
+}
+
+vector<vector<State>> ReadBoardFile(string path) {
+	vector<vector<State>> board;
+	// Read one line call the 
+	string line;
+	fstream myfile(path);
+	if (myfile) {
+		string line;
+		while (getline(myfile, line)) {
+			vector<State> row = ParseLine(line);
+			board.push_back(row);
+		}
+	}
+	return board;
+}
+
+string CellString(State cell) {
+	switch (cell) {
+	case State::Kobstcale:
+		return "1";
+	case State::Kempty:
+		return "0";
+	}
+}
+
+void PrintBoard(const vector<vector<State>> board) { 
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[i].size(); j++) {
+			cout << CellString(board[i][j]);
+		}
+		cout << "\n";
+	}
+}
+
+int main()
+{   
+	std::cout << "Hello World!\n"; 
+	auto board = ReadBoardFile("D:\\course\\udacity\\CPP_Nanodegreecodes\\SAUdacityCppNanodegree-master\\lesson_examples\\Foundations\\1.board");
+	PrintBoard(board) ;
+}
+
+
